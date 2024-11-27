8f358fa3e6155497761924a2f9ef9638abfe67c4
Emanuel Capurro <hola@emanuelcapurro.com>
2024-11-27 17:36:09 -0300

    Añadido PWA

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/pld-poc/dev-sw.js?dev-sw', { scope: '/pld-poc/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+      manifest: {
+        name: 'KIM PLD',
+        short_name: 'KimPLD',
+        description: 'Planilla Diario de Kantar IBOPE Media',
+        theme_color: '#512bd4',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   base: 'https://ecapurro-ok.github.io/pld-poc/',
   resolve: {
