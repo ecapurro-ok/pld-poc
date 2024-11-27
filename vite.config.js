9fef896a677b21cbbe97518bef274dcbfd943ef8
Emanuel Capurro <hola@emanuelcapurro.com>
2024-11-27 16:45:28 -0300

    modifique vite config sin base url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'https://ecapurro-ok.github.io/pld-poc',
+  // base: 'https://ecapurro-ok.github.io/pld-poc',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
